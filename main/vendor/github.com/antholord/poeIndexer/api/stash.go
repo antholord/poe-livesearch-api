8945d5cceaeccdbc03882f475cde60ad34517044
Anthony Lord <antholord@hotmail.com>
2017-04-12 15:58:43 -0400

    second commit

---
+++ main/vendor/github.com/antholord/poeIndexer/api/stash.go
@@ -0,0 +1,12 @@
+//Credit to https://github.com/ccbrown/poe-go/
+package api
+
+type Stash struct {
+	AccountName       string `json:"accountName"`
+	LastCharacterName string `json:"lastCharacterName"`
+	Id                string `json:"id"`
+	Label             string `json:"stash"`
+	Type              string `json:"stashType"`
+	Items             []Item `json:"items"`
+	IsPublic          bool   `json:"public"`
+}
